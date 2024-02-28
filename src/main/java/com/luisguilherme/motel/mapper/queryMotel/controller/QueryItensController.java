5284563e94e233990cd1850818ae4d5e5298171a
lgfas <luis.guilherme2811@gmail.com>
2024-02-28 12:03:08 -0300

    QueryItens -> criar, atualizar, listar e deletar itens já implementados. Modificando para utilizar corretamente o Page em obterItens()

--- src/main/java/com/luisguilherme/motel/mapper/queryMotel/controller/QueryItensController.java
+++ src/main/java/com/luisguilherme/motel/mapper/queryMotel/controller/QueryItensController.java
@@ -2,11 +2,13 @@ package com.luisguilherme.motel.mapper.queryMotel.controller;
 
 import com.luisguilherme.motel.mapper.queryMotel.model.QueryItens;
 import com.luisguilherme.motel.mapper.queryMotel.service.QueryItensService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/queryItens")
@@ -20,7 +22,28 @@ public class QueryItensController {
 
     @PostMapping("/criarItem")
     @ResponseStatus(HttpStatus.CREATED)
-    public QueryItens criarItem(QueryItens queryItens) {
-        return queryItensService.criarItem(queryItens);
+    public void criarItem(QueryItens queryItens) {
+        queryItensService.criarItem(queryItens);
     }
+
+    @GetMapping("/obterItens")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<QueryItens> obterItens (@RequestParam(defaultValue = "0") int page,
+                                        @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return queryItensService.obterItens(pageable, page, size);
+    }
+
+    @PutMapping("/atualizarItem")
+    @ResponseStatus(HttpStatus.OK)
+    public void atualizarItem(QueryItens queryItens) {
+        queryItensService.atualizarItem(queryItens);
+    }
+
+    @DeleteMapping("/deletarItem/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarItem (@PathVariable Long id) {
+        queryItensService.deletarItem(id);
+    }
+
 }
--- src/main/java/com/luisguilherme/motel/mapper/queryMotel/model/QueryItens.java
+++ src/main/java/com/luisguilherme/motel/mapper/queryMotel/model/QueryItens.java
@@ -1,7 +1,11 @@
 package com.luisguilherme.motel.mapper.queryMotel.model;
 
 public record QueryItens(
+        Long id,
         String descricao,
         Float valor
 ) {
+
+
+
 }
