a033799291405b73dff26da946efb5375f5b8117
lgfas <luis.guilherme2811@gmail.com>
2024-03-06 11:04:24 -0300

    RelatorioServidorService, RelatorioServidorController e cracha-servidor.jrxml alterados para gerar pdf corretamente

--- src/main/java/com/luisguilherme/motel/relatorio/RelatorioServidorController.java
+++ src/main/java/com/luisguilherme/motel/relatorio/RelatorioServidorController.java
@@ -2,10 +2,7 @@ package com.luisguilherme.motel.relatorio;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/relatorio")
@@ -19,7 +16,8 @@ public class RelatorioServidorController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public void servidorRelatorio(Integer codServidor, HttpServletResponse response) throws Exception {
+    public void servidorRelatorio(@RequestParam Integer codServidor, HttpServletResponse response) throws Exception {
+
         relatorioServidorService.servidorRelatorio(codServidor, response);
     }
 }
